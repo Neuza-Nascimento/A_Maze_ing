58a5049a297aa9aac115058602fd72fc143600a5
Neuza Teresa Do Nascimento <nedo-nas@c1r6s5.42porto.com>
2026-08-17 18:31:59 +0100

    solution path done

--- src/mazegen/maze_generator.py
+++ src/mazegen/maze_generator.py
@@ -39,3 +39,65 @@ class MazeGenerator:
             self.grid.append(row)
 
     def _42_pattern(self) -> None: ...
+
+    def _solution_path(self) -> list[str]:
+        from collections import deque
+        from enum import Enum
+
+        NORTH = 0b0001
+        EAST = 0b0010
+        SOUTH = 0b0100
+        WEST = 0b1000
+
+        DIRECTIONS: dict[int, tuple[int, int]] = {
+            NORTH: (0, -1),
+            EAST: (1, 0),
+            SOUTH: (0, 1),
+            WEST: (-1, 0)
+        }
+
+        DIR_LETTER: dict[int, str] = {
+            NORTH: "N",
+            EAST:  "E",
+            SOUTH: "S",
+            WEST:  "W"
+        }
+
+        queque: deque[tuple[tuple[int, int], list[any]]] = deque(
+            [(self.config.entry, [])])
+
+        walls_visited: set[tuple[int, int]] = {self.config.entry}
+
+        while queque:
+            position, path = queque.popleft()
+            px, py = position
+
+            if position == self.config.exit:
+                return path
+
+            for direction, (dx, dy) in DIRECTIONS.items():
+                nx = dx + px
+                ny = dy + py
+
+                if nx < 0 or nx >= self.width:
+                    continue
+
+                if ny < 0 or ny >= self.height:
+                    continue
+
+                if (nx, ny) in walls_visited:
+                    continue
+
+                if self.grid[px][py] & direction:
+                    continue
+
+                walls_visited.add((nx, ny))
+                queque.append((nx, ny), path + DIR_LETTER[direction])
+
+        return []
+
+    def output_res(self) -> None:
+        with open("output.txt", "w"):
+            for i in self.grid:
+                for j in self.grid:
+                    
