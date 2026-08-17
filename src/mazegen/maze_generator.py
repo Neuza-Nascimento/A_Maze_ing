8bc0b99b5a8ca68e4d041b4fda9d46d9403a97c4
Luzzie <estelarodrigoluz@gmail.com>
2026-08-17 18:38:32 +0100

    pattern exists now yeyyeyeyyee

--- src/mazegen/maze_generator.py
+++ src/mazegen/maze_generator.py
@@ -1,6 +1,8 @@
 import random
 from typing import TYPE_CHECKING
 
+from magic_values import P42, MagicValues
+
 if TYPE_CHECKING:
     from mazegen import MazeConfig
 
@@ -10,32 +12,34 @@ class MazeGenerator:
         self.config: MazeConfig = config
         self.width: int = config.width
         self.height: int = config.height
-        self.center: tuple[int, int] = (
-            int(self.width / 2),
-            int(self.height / 2),
-        )
         self.grid: list[list[int]] = []
-
+        self.blocked: set = set()
         random.seed(config.seed)
         self._create_grid()
 
     def _create_grid(self) -> None:
-        ex, ey = self.config.entry
-        sx, sy = self.config.exit
-        cx, cy = self.center
-        for i in range(self.height):
-            row: list[int] = []
-            for j in range(self.width):
-                if i == ey and j == ex:
-                    row.append(0b0010)
-                    continue
-                if i == sy and j == sx:
-                    row.append(0b0010)
-                    continue
-                if cy in {i, j} or cx in {j, i}:
-                    row.append(0b0001)
-                    continue
-                row.append(0b0000)
+        n: int = MagicValues.NORTH.value
+        for _ in range(self.height):
+            row: list[int] = [n for _ in range(self.width)]
             self.grid.append(row)
+        self._42_pattern()
 
-    def _42_pattern(self) -> None: ...
+    def _42_pattern(self) -> bool:
+        center_x = (self.width // 2) - 5
+        center_y = (self.height // 2) - 4
+        for i in range(1, self.height - 1):
+            row = self.grid[i]
+            for j in range(1, self.width - 1):
+                row[j] = MagicValues.OPEN.value
+
+        for (dx, dy) in P42:
+            x = center_x + dx
+            y = center_y + dy
+            if (x < 0 or x >= self.width or y < 0 or y >= self.height):
+                print("Warning: Maze too small for 42 pattern!")
+                return False
+            self.grid[y][x] = MagicValues.PATTERN.value
+            self.blocked.add((x, y))
+        return True
+
+    def _carve_maze(self) -> None: ...
