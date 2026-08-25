6da0514205936ea9da2f41e74afb69363987158d
Luzzie-ux <estelarodrigoluz@gmail.com>
2026-08-25 18:16:11 +0100

    border problem solved

--- src/mazegen/maze_generator.py
+++ src/mazegen/maze_generator.py
@@ -51,7 +51,9 @@ class MazeGenerator:
         for y in range(self._height):
             row: list[int] = []
             for x in range(self._width):
-                if y in {0, self._height - 1} or x in {0, self._width - 1}:
+                if self.config.perfect and (
+                    y in {0, self._height - 1} or x in {0, self._width - 1}
+                ):
                     self._blocked.add((x, y))
                 row.append(n)
             self._grid.append(row)
